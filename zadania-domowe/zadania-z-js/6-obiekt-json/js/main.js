459bce1a67bdeee133bdc7baccf24ee93053d0e8
michalandrzejak <michal_and@interia.eu>
2019-08-24 10:47:21 +0200

    zadania-domowe

--- zadania-domowe/zadania-z-js/6-obiekt-json/js/main.js
+++
@@ -1,8 +0,0 @@
-
-let jsonPracownicy = {
-    "pracownicy":[
-        {"firstName": "Krystian", "lastName": "Dziopa"}, 
-        {"firstName": "Anna", "lastName": "Szapiel"},
-        {"firstName": "Piotr", "lastName": "Żmuda"}
-    ]
-}
